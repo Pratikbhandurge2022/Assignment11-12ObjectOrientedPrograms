e18b4755defcb0731a844807506bc1e891b1fe3b
“pratik” <pratikbhandurge2222@gmail.com>
2022-08-16 15:55:24 +0530

    DeckOfCards

---
+++ DeckOfCards/Card.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckOfCards
+{
+    internal class Class1
+    {
+    }
+}
---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ObjectOrientedProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  CardsSimulation simulation = new CardsSimulation();
+            //  simulation.DriverMethod();
+
+            DeckOfCards deckOfCards = new DeckOfCards();
+            deckOfCards.Shuffle();
+
+        }
+    }
+}
