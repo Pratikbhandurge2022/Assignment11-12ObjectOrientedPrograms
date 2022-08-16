2868a31c0fdcb6cd30aafc9aa1a227fcd364d6dc
“pratik” <pratikbhandurge2222@gmail.com>
2022-08-16 15:48:19 +0530

    StocksManagemnet

---
+++ StocksManagement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectOrientedProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 for AddressBook\n" +
+               "Enter 2 for InventoryManagement\n" +
+               "Enter 3 For Stock Management\n"
+               );
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 3:
+                    StocksManagement.DriverMethod();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+
+            }
+
+        }
+    }
+}
